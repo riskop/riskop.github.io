4255ba2571af6b775d635552052baa6e9a1ecf72
riskop <peter@risko.hu>
2022-02-04 13:07:57 +0100

    Update sandbox.js

--- sandbox/sandbox.js
+++ sandbox/sandbox.js
@@ -1,5 +1,21 @@
 console.log('Hello from JS!');
 
+function curry(fn) {
+  const args = []
+  return function inner(arg) {
+    if(args.length === fn.length) return fn(...args)
+    args.push(arg)
+    return inner
+  }
+}
+
+function add(a, b) {
+  return a + b
+}
+
+
 function buttonPressed() {
     console.log('xxxx');
+    const curriedAdd = curry(add)
+    console.log(curriedAdd(2)(3)()) // 5
 };
